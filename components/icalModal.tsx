1b9f3ce38ad89f6d6e719b024c118ca072e6d9fc
Haider Ali Punjabi <me@haideralipunjabi.com>
2024-03-12 02:41:09 +0530

    Fxied calendar script

--- components/icalModal.tsx
+++ components/icalModal.tsx
@@ -7,52 +7,52 @@ import Link from "next/link";
 import { OffsetType } from "@/types";
 
 export default function ICalModal({
-  name,
-  offsets,
+    name,
+    offsets,
 }: {
-  name: string;
-  offsets: Array<OffsetType>;
+    name: string;
+    offsets: Array<OffsetType>;
 }) {
-  const [hidden, setHidden] = useState(true);
-  const button = (
-    <button
-      onClick={() => {
-        setHidden(false);
-      }}
-      className="text-2 mx-auto w-min rounded-full bg-secondary px-16 py-2"
-    >
-      Download iCal
-    </button>
-  );
-  if (hidden) return button;
-  return (
-    <>
-      {button}
-      <ModalOverlay />
-      <ModalCard>
-        <ModalHeader>
-          <h2 className="text-3 text-center text-white">Calendars</h2>
-          <button
-            className="text-2 absolute right-5 top-2 text-white"
+    const [hidden, setHidden] = useState(true);
+    const button = (
+        <button
             onClick={() => {
-              setHidden(true);
+                setHidden(false);
             }}
-          >
-            <FontAwesomeIcon icon={faTimes} />
-          </button>
-        </ModalHeader>
-        <ModalBody>
-          {offsets.map((offset, key) => (
-            <Link
-              className="text-3 text-white underline hover:cursor-pointer"
-              key={key}
-              href={`/ical/${name}-${offset.offset}.ical`}
-            >
-              {offset.name}
-            </Link>
-          ))}
-        </ModalBody>
-      </ModalCard>
-    </>
-  );
+            className="text-2 mx-auto w-min rounded-full bg-secondary px-16 py-2"
+        >
+            Download iCal
+        </button>
+    );
+    if (hidden) return button;
+    return (
+        <>
+            {button}
+            <ModalOverlay />
+            <ModalCard>
+                <ModalHeader>
+                    <h2 className="text-3 text-center text-white">Calendars</h2>
+                    <button
+                        className="text-2 absolute right-5 top-2 text-white"
+                        onClick={() => {
+                            setHidden(true);
+                        }}
+                    >
+                        <FontAwesomeIcon icon={faTimes} />
+                    </button>
+                </ModalHeader>
+                <ModalBody>
+                    {offsets.map((offset, key) => (
+                        <Link
+                            className="text-3 text-white underline hover:cursor-pointer"
+                            key={key}
+                            href={`/ical/${name}-${offset.offset}.ics`}
+                        >
+                            {offset.name}
+                        </Link>
+                    ))}
+                </ModalBody>
+            </ModalCard>
+        </>
+    );
 }
